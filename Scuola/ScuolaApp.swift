c73ccbe6a9cae582f4873f0a1058bc3685b95391
bradenross <bradenro55@yahoo.com>
2023-06-29 22:52:40 -0500

    Initial Commit

---
+++ Scuola/ScuolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScuolaApp.swift
+//  Scuola
+//
+//  Created by Braden Ross on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ScuolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
