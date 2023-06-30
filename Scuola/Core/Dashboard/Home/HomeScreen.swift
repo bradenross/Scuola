73a8c62ebec89062af1b27f5152810fb89b7fb96
bradenross <bradenro55@yahoo.com>
2023-06-29 23:38:54 -0500

    Added home screen

---
+++ Scuola/Core/Dashboard/Home/HomeScreen.swift
@@ -0,0 +1,35 @@
+//
+//  HomeScreen.swift
+//  Scuola
+//
+//  Created by Braden Ross on 6/29/23.
+//
+
+import SwiftUI
+import Firebase
+import AVKit
+
+struct HomeScreen: View {
+    
+    
+    var body: some View {
+        VStack(){
+            Text("Test")
+        }
+        
+    }
+    
+    func Logout(){
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
