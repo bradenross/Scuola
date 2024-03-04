c21abc98346197c562e099ee40c19a6f8a509720
bradenross <bradenro55@yahoo.com>
2024-03-03 23:21:39 -0600

    index on main: 40621ed Added frameworks for inbox, fixed dashboard issue with too many cases

---
+++ Scuola/Data/Session/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  Scuola
+//
+//  Created by Braden Ross on 3/3/24.
+//
+
+import Foundation
---
+++ Scuola/Presentation/Authentication/VerifyPage.swift
@@ -0,0 +1,18 @@
+//
+//  VerifyPage.swift
+//  Scuola
+//
+//  Created by Braden Ross on 3/3/24.
+//
+
+import SwiftUI
+
+struct VerifyPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerifyPage()
+}
